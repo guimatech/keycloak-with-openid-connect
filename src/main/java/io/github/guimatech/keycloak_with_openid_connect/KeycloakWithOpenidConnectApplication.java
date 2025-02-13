faebbc67994c87c38efe231c7a308e624925ed80
Lucas dos Santos Guimarães <guimaraes-lucas@outlook.com.br>
2025-02-12 22:42:53 -0300

    chore: start spring project

---
+++ src/main/java/io/github/guimatech/keycloak_with_openid_connect/KeycloakWithOpenidConnectApplication.java
@@ -0,0 +1,13 @@
+package io.github.guimatech.keycloak_with_openid_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakWithOpenidConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakWithOpenidConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/guimatech/keycloak_with_openid_connect/KeycloakWithOpenidConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.guimatech.keycloak_with_openid_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakWithOpenidConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
